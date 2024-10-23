2e41392cbd9a81784a6e69e65cbf9f2d6ffd2f01
Roald Ruiter <roaldruiter@gmail.com>
2024-10-23 09:08:16 +0200

    add auto merge script

---
+++ _tools/auto_merge/main.py
@@ -0,0 +1,21 @@
+import os
+from datetime import datetime
+from github import Github
+
+# Authenticate with GitHub
+g = Github(os.getenv('GITHUB_TOKEN'))
+repo = g.get_repo(os.getenv('GITHUB_REPOSITORY'))
+prs = repo.get_pulls(state='open')
+
+today = datetime.today().strftime('%Y-%m-%d')
+
+for pr in prs:
+    # Check if the markdown file exists
+    files = pr.get_files()
+    for file in files:
+        if file.filename.startswith('_posts/') and file.filename.endswith('.md'):
+            post_date = file.filename.split('/')[1].split('-')[:3]
+            post_date_str = '-'.join(post_date)
+            if post_date_str == today:
+                pr.merge(merge_method='squash')
+                print(f'Merged PR #{pr.number} with post {file.filename}')
